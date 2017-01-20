548961560c6c62788ed72d31dae1351d9b91d8d4
Daniel Tsang <danieltsang94@gmail.com>
2017-01-19 21:40:35 -0800

    Revamp to make it brighter and more interesting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Glyphicon } from 'react-bootstrap';
+import { Glyphicon, ResponsiveEmbed } from 'react-bootstrap';
 import './App.css';
 
 import Experience from './components/experience/experience';
@@ -9,6 +9,13 @@ import SideBySideSection from './components/side-by-side-section/side-by-side-se
 import avatar from './images/avatar.jpg';
 import hootsuiteLogo from './images/hootsuitelogo.png';
 import indochinoLogo from './images/indochinologo.png';
+
+import bowling from './images/bowling.svg';
+import bulldog from './images/french-bulldog.svg';
+import shepherd from './images/police-dog.svg';
+import pug from './images/pug.svg';
+import soccer from './images/soccer-ball-variant.svg';
+import watermelon from './images/watermelon.svg';
 
 class App extends Component {
     constructor() {
@@ -29,7 +36,8 @@ class App extends Component {
     render () {
         const glyphiconStyles = {
             fontSize: "30px",
-            margin: "0 8px 0 0"
+            margin: "0 8px 0 0",
+            color: "#000000"
         };
         return (
             <div className="App">
@@ -41,11 +49,9 @@ class App extends Component {
                     </div>
                     <div className="App-header-right">
                         <a href="https://github.com/Danieltsang?tab=repositories" className="App-header-button">
-                            <Glyphicon glyph="wrench" style={glyphiconStyles} />
                             Work
                         </a>
                         <h3 className="App-header-button" onClick={this.scrollSectionIntoView.bind(this, "contact")}>
-                            <Glyphicon glyph="send" style={glyphiconStyles} />
                             Contact
                         </h3>
                     </div>
@@ -55,33 +61,15 @@ class App extends Component {
                     <Glyphicon
                         glyph="menu-down"
                         style={glyphiconStyles}
-                        onClick={this.scrollSectionIntoView.bind(this, "aboutMe")}
                     />
                 </div>
                 <div className="App-body">
-                    <SideBySideSection title="About me" ref={(node) => this.setReference(node, "aboutMe")}>
-                        <p>Who: Daniel Tsang</p>
-                        <p>What: Front End Engineer</p>
-                        <p>Why: New trends and technologies keep the front end landscape super engaging and fascinating</p>
-                        <p>Where: Beautiful Vancouver, British Columbia, Canada</p>
-                    </SideBySideSection>
-                    <div className="App-divider"></div>
-                    <SideBySideSection title="Skills" ref={(node) => this.setReference(node, "skills")}>
-                        <ul>
-                            <li>Vanilla Javascript</li>
-                            <li>React</li>
-                            <li>jQuery</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>LESS</li>
-                            <li>PHP</li>
-                            <li>Java</li>
-                            <li>Jasmine</li>
-                        </ul>
+                    <SideBySideSection title="Who Am I?" ref={(node) => this.setReference(node, "aboutMe")}>
+                        <p>Hi! My name is Daniel Tsang and I am an undergraduate at the University of British Columbia graduating in May 2017. Currently, I am searching for a job as a Front End Engineer in the Big Apple. The combination of the growing tech industry and my passion for coding invigorates me to travel from Vancouver, Canada to New York to kickoff my career.</p>
                     </SideBySideSection>
                     <div className="App-divider"></div>
                     <div className="App-experience" ref={(node) => this.setReference(node, "experience")}>
-                        <h2>Experiences</h2>
+                        <h2>Who Have I Worked For?</h2>
                         <div className="App-experience-container">
                             <Experience
                                 companyUrl="www.hootsuite.com"
@@ -102,11 +90,79 @@ class App extends Component {
                         </div>
                     </div>
                     <div className="App-divider"></div>
+                    <SideBySideSection title="What Can I Do?" ref={(node) => this.setReference(node, "skills")}>
+                        <ul>
+                            <li>Vanilla Javascript</li>
+                            <li>React</li>
+                            <li>jQuery</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>LESS</li>
+                            <li>PHP</li>
+                            <li>Java</li>
+                            <li>Jasmine</li>
+                        </ul>
+                    </SideBySideSection>
+                    <div className="App-divider"></div>
+                    <SideBySideSection title="What Do I Do Well?" ref={(node) => this.setReference(node, "skills")}>
+                        <ul>
+                            <li>Adapt</li>
+                            <li>Work Independently</li>
+                            <li>Dedication</li>
+                        </ul>
+                    </SideBySideSection>
+                    <div className="App-divider"></div>
+                    <SideBySideSection
+                        title="What Do I Like?"
+                        ref={(node) => this.setReference(node, "skills")}
+                    >
+                        <div className="row">
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={bowling} />
+                                </ResponsiveEmbed>
+                            </div>
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={soccer} />
+                                </ResponsiveEmbed>
+                            </div>
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={watermelon} />
+                                </ResponsiveEmbed>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <h3>My Dogs</h3>
+                        </div>
+                        <div className="row">
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={bulldog} />
+                                </ResponsiveEmbed>
+                                Dagan
+                            </div>
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={shepherd} />
+                                </ResponsiveEmbed>
+                                Zelda
+                            </div>
+                            <div className="svg">
+                                <ResponsiveEmbed a16by9>
+                                    <embed type="image/svg+xml" src={pug} />
+                                </ResponsiveEmbed>
+                                Tia
+                            </div>
+                        </div>
+                    </SideBySideSection>
+                    <div className="App-divider"></div>
                     <div className="App-contact" ref={(node) => this.setReference(node, "contact")}>
                         <h1>I have what it takes to be a great employee</h1>
                         <p>I look to begin my career as a Front End Engineer from May 2017 and onwards.</p>
                         <a href="mailto:danieltsang94@gmail.com">
-                            Send me a message
+                            Let's Connect
                         </a>
                     </div>
                 </div>
--- src/components/side-by-side-section/side-by-side-section.js
+++ src/components/side-by-side-section/side-by-side-section.js
@@ -3,10 +3,13 @@ import './side-by-side-section.css';
 
 class SideBySideSection extends Component {
     render () {
+        let styles = {
+            flexDirection: this.props.orientation
+        };
         return (
             <div className="SideBySideSection">
                 <h2>{this.props.title}</h2>
-                <div className="SideBySideSection-content">
+                <div className="SideBySideSection-content" style={styles}>
                     {this.props.children}
                 </div>
             </div>
@@ -16,7 +19,12 @@ class SideBySideSection extends Component {
 
 SideBySideSection.propTypes = {
     children: React.PropTypes.any.isRequired,
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    orientation: React.PropTypes.oneOf(['row', 'column'])
+};
+
+SideBySideSection.defaultProps = {
+    orientation: 'column'
 };
 
 export default SideBySideSection;
